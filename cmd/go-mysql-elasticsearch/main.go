a61cef46ce656a14fc3e0c557e40021722ce1bae
Nicholas Terrell <nicholas.terrell@mnfgropup.limited>
2018-09-11 11:44:41 +1000

    updated import path

--- cmd/go-mysql-elasticsearch/main.go
+++ cmd/go-mysql-elasticsearch/main.go
@@ -9,7 +9,7 @@ import (
 
 	"github.com/juju/errors"
 	"github.com/siddontang/go-log/log"
-	"github.com/siddontang/go-mysql-elasticsearch/river"
+	"github.com/spid37/go-mysql-elasticsearch/river"
 )
 
 var configFile = flag.String("config", "./etc/river.toml", "go-mysql-elasticsearch config file")
